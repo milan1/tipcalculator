e1b726ef4d58159c662c5d77b083493bea81e92c
Milan Brahmbhatt <mbrahmbhatt@zynga.com>
2013-10-02 15:05:29 -0700

    first commit

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Milan Brahmbhatt on 10/2/13.
+//  Copyright (c) 2013 Milan Brahmbhatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
